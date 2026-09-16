6b6ed165e9088c3ceb52d4cbd09440043951f7b7
Martin Fernandez <ricardomartinfernandez94@gmail.com>
2026-09-16 20:27:00 -0300

    Actualiza carta

--- js/menu.js
+++ js/menu.js
@@ -31,66 +31,25 @@ const carta = [
         ]
     },
 
-    {
-        categoria: "COMIDAS",
-        columna: "izquierda",
+{
+    categoria: "COMIDAS",
+    columna: "izquierda",
+    productos: [
+        { nombre: "PIZZA", precio: 11000 },
+        { nombre: "MUZARELLA", precio: 11000 },
+        { nombre: "MUZARELLA & JAMÓN", precio: 12000 },
+        { nombre: "MUZARELLA & SALAME", precio: 13000 },
+        { nombre: "MUZARELLA INTEGRAL", precio: 12000 },
+        { nombre: "SUPERPANCHO", precio: 3500 },
+        { nombre: "EMPANADAS", descripcion: "JAMÓN, QUESO / CARNE", precio: 1000 },
+        { nombre: "TOSTADO X 6", precio: 7000 },
+        { nombre: "SANDWICH MIGAS 6", precio: 6000 },
+        { nombre: "PEBETE", precio: 1500 },
+        { nombre: "CHIPÁ X 100 g", precio: 1700 },
+        { nombre: "MEDIALUNAS", precio: 1000 }
+    ]
+},
 
-        productos: [
-            {
-                nombre: "PIZZA",
-                precio: 11000
-            },
-            {
-                nombre: "MUZARELLA",
-                precio: 11000
-            },
-            {
-                nombre: "MUZARELLA & JAMÓN",
-                precio: 12000
-            },
-            {
-                nombre: "MUZARELLA & SALAME",
-                precio: 13000
-            },
-            {
-                nombre: "MUZARELLA INTEGRAL",
-                precio: 12000
-            },
-            {
-                nombre: "EMPANADAS",
-                descripcion: "JAMÓN, QUESO / CARNE",
-                precio: 1000
-            }
-        ]
-    },
-
-    {
-        categoria: "SUPERPANCHO",
-        columna: "izquierda",
-
-        productos: [
-            {
-                nombre: "TOSTADO X 6",
-                precio: 7000
-            },
-            {
-                nombre: "SANDWICH MIGAS 6",
-                precio: 6000
-            },
-            {
-                nombre: "PEBETE",
-                precio: 1500
-            },
-            {
-                nombre: "CHIPÁ X 100 g",
-                precio: 1700
-            },
-            {
-                nombre: "MEDIALUNAS",
-                precio: 1000
-            }
-        ]
-    },
 
     {
         categoria: "BAR",
